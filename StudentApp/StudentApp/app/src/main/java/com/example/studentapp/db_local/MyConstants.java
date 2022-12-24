bf8dad8c7aa64179920b3c33affab192749d3126
pichugin-i <pichugin.lua@yandex.ru>
2022-12-24 17:40:32 +0300

    Add v 24.12.22 17:40 pichugin

--- StudentApp/StudentApp/app/src/main/java/com/example/studentapp/db_local/MyConstants.java
+++ StudentApp/StudentApp/app/src/main/java/com/example/studentapp/db_local/MyConstants.java
@@ -1,45 +1,61 @@
 package com.example.studentapp.db_local;
 
+import com.example.studentapp.db.Plan;
+import com.example.studentapp.db.Questions;
+import com.example.studentapp.db.Subjects;
+
 public class MyConstants {
     // При изменении в бд +1
-    private static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 1;
 
     // Название бд
-    private static final String DATABASE_NAME = "smart_students";
+    public static final String DB_NAME = "smart_students";
 
     // Название таблиц в бд
-    private static final String TABLE_PLAN = "plan";
-    private static final String TABLE_QUESTION = "question";
-    private static final String TABLE_SUBJECT = "subject";
+    public static final String TABLE_PLAN = "plan";
+    public static final String TABLE_QUESTION = "question";
+    public static final String TABLE_SUBJECT = "subject";
 
     // Общие колонны таблиц SUBJECT и QUESTION
-    private static final String KEY_SUB_ID = "sub_id";
-    private static final String KEY_IS_COMPLETED = "is_completed";
+    public static final String KEY_SUB_ID = "sub_id";
+
 
     // Название колонн для таблицы TABLE_PLAN
-    private static final String KEY_PLAN_ID = "plan_id";
-    private static final String KEY_DATE_PLAN = "date_plan";
-    private static final String KEY_NUM_QUE_PLAN = "num_que_plan";
+    public static final String KEY_PLAN_ID = "plan_id";
+    public static final String KEY_DATE_PLAN = "date_plan";
+    public static final String KEY_NUM_QUE_PLAN = "num_que_plan";
 
     // Название колонн для таблицы TABLE_QUESTION
-    private static final String KEY_QUESTION_ID = "question_id";
-    private static final String KEY_TEXT_QUESTION = "text_question";
-    private static final String KEY_TEXT_ANSWER = "text_answer";
+ // private static final String KEY_SUB_ID = "sub_id";
+    public static final String KEY_QUESTION_ID = "question_id";
+    public static final String KEY_TEXT_QUESTION = "text_question";
+    public static final String KEY_TEXT_ANSWER = "text_answer";
+    public static final String KEY_DATE_QUESTION = "date_question";
+    public static final String KEY_SIZE_OF_VIEW = "size_of_view";
+    public static final String KEY_PERCENT_KNOW = "percent_know";
+
 
     // Название колонн для таблицы TABLE_SUBJECT
-    private static final String KEY_SUBJECT_NAME = "subject_name";
-    private static final String KEY_SUBJECT_DATE = "subject_date";
-    private static final String KEY_USER_ID = "user_id";
+ // private static final String KEY_SUB_ID = "sub_id";
+    public static final String KEY_SUBJECT_NAME = "subject_name";
+    public static final String KEY_SUBJECT_DATE = "subject_date";
+    public static final String KEY_USER_ID = "user_id";
+    public static final String KEY_TODAY_LEARNED = "today_learned";
 
-    private static final String CREATE_TABLE_PLAN = "CREATE TABLE " + TABLE_PLAN
+    public static final String CREATE_TABLE_PLAN = "CREATE TABLE " + TABLE_PLAN
             + "(" + KEY_PLAN_ID + " INTEGER PRIMARY KEY," + KEY_DATE_PLAN + " TEXT,"
             + KEY_NUM_QUE_PLAN + " INTEGER," + KEY_SUB_ID + " INTEGER" + ")";
 
-    private static final String CREATE_TABLE_QUESTION = "CREATE TABLE " + TABLE_QUESTION
+    public static final String CREATE_TABLE_QUESTION = "CREATE TABLE " + TABLE_QUESTION
             + "(" + KEY_QUESTION_ID + " INTEGER PRIMARY KEY," + KEY_TEXT_QUESTION + " TEXT,"
-            + KEY_TEXT_ANSWER + " TEXT," + KEY_IS_COMPLETED + " BOOLEAN," + KEY_SUB_ID + " INTEGER" + ")";
+            + KEY_TEXT_ANSWER + " TEXT," + KEY_PERCENT_KNOW + " INTEGER," +  KEY_DATE_QUESTION + " INTEGER,"
+            + KEY_SIZE_OF_VIEW + " INTEGER," + KEY_SUB_ID + " INTEGER" + ")";
 
-    private static final String CREATE_TABLE_SUBJECT = "CREATE TABLE " + TABLE_SUBJECT
+    public static final String CREATE_TABLE_SUBJECT = "CREATE TABLE " + TABLE_SUBJECT
             + "(" + KEY_SUB_ID + " INTEGER PRIMARY KEY," + KEY_SUBJECT_NAME + " TEXT,"
-            + KEY_SUBJECT_DATE + " TEXT," + KEY_IS_COMPLETED + " BOOLEAN," + KEY_USER_ID + " INTEGER" + ")";
+            + KEY_SUBJECT_DATE + " TEXT," + KEY_TODAY_LEARNED + " INTEGER," + KEY_USER_ID + " INTEGER" + ")";
+
+    public static final String DROP_TABLE_PLAN = "DROP TABLE IF EXISTS " + TABLE_PLAN;
+    public static final String DROP_TABLE_QUESTION = "DROP TABLE IF EXISTS " + TABLE_QUESTION;
+    public static final String DROP_TABLE_SUBJECT = "DROP TABLE IF EXISTS " + TABLE_SUBJECT;
 }
